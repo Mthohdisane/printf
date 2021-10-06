8e6c2cd240f826af3f43d6cde795d6f01167b45e
Protogene Mugiraneza <50316270+protee1@users.noreply.github.com>
2021-10-06 19:44:23 +0200

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _printf - receive the main string and all arguments
+ * to print a formated string
+ * @format: string containing all the desired characters
+ * Return: the number of characters printed (without the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+
+	convert_t f_list[BUFFSIZE] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_decimal},
+		{"i", print_integer},
+		{"x", _hex_l},
+		{"X", _hex_u},
+		{"p", _hex_l},
+		{"S", print_string},
+		{"r", print_r},
+		{"R", rot_13},
+		{"b", print_binary},
+		{"u", print_unint},
+		{"o", print_octal},
+		{NULL, NULL},
+	};
+	va_list arg_list;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	va_start(arg_list, format);
+	/*call parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
+}
